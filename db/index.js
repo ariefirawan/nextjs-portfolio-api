2b5b9415309959724c7ee1f5dcd970915f6c2057
arief irawan <ariefirawant@gmail.com>
2020-09-04 13:03:20 +0700

    connect with mongoose

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const config = require('../config/db');
+
+exports.connect = () => {
+  return mongoose.connect(
+    config.DB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify:false
+    },
+    (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Connected to database');
+      }
+    }
+  );
+};
